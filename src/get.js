2ec98750b62c9b708cf0a9564515d75ca90a5ede
rsol7 <rsolm72@gmail.com>
2018-11-13 04:39:03 -0700

    Get API, serverless config & mock file

---
+++ src/get.js
@@ -0,0 +1,31 @@
+import * as dynamoDbLib from '../libs/dynamo-lib';
+import { success, failure } from '../libs/response-lib';
+
+export async function main(event, context){
+    const params = {
+        TableName: "notes",
+        // 'Key' defines the partition key and sort key of the item to be retrieved
+        // - 'userId': Identity Pool identity id of the authenticated user
+        // - 'noteId': path parameter
+
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call("get", params);
+        if(result.Item){
+            return success(result.Item);
+        } else {
+            return failure({status: true, error: "Item not found."});
+        }
+    } catch (e) {
+        console.log("error", e);
+        return failure({status: false});
+    }
+}
+
+// in get-event.json
+//make sure to reference correct id made by AWS
